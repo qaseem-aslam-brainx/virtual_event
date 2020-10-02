d2d95f6697203eb7f8b73f74552cffa201d1ca89
Qaseem Aslam <qaseem.aslam@brainxtech.com>
2020-10-02 19:59:28 +0500

    Added controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,53 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    @event.user_id = session[:user_id]
+
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :logo, :date, :time_start, :time_end, :user_id)
+  end
+end
