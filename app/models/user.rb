8354d8eaa42a542695262017c0a09a20e0d1e19b
Qaseem Aslam <qaseem.aslam@brainxtech.com>
2020-10-07 15:26:09 +0500

    made changes metioned in PR

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email, presence: true, uniqueness: true,
-            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-  validates :encrypted_password, :first_name, :last_name, presence: true
+  validates :first_name, :last_name, presence: true
 end
