6309d62de2fb53e3dcd2208078ef6c7985c20612
Qaseem Aslam <qaseem.aslam@brainxtech.com>
2020-10-07 11:30:59 +0500

    changes in index and events css

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
 
-    @event.user_id = session[:user_id]
+    @event.user_id = current_user.id
 
     if @event.save
       redirect_to @event
