a08900b81282b7e969aca628eb8254d49768ef0b
Qaseem Aslam <qaseem.aslam@brainxtech.com>
2020-10-12 14:48:11 +0500

    Fixed login page responsiveness issue

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_10_09_090251) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 2020_10_09_090251) do
     t.string "reference"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "event_id", null: false
+    t.bigint "event_id", null: false
     t.index ["event_id"], name: "index_attendees_on_event_id"
   end
 
@@ -55,7 +58,7 @@ ActiveRecord::Schema.define(version: 2020_10_09_090251) do
     t.date "date"
     t.time "time_start"
     t.time "time_end"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status"
@@ -67,7 +70,7 @@ ActiveRecord::Schema.define(version: 2020_10_09_090251) do
     t.string "name"
     t.string "website"
     t.integer "status"
-    t.integer "event_id", null: false
+    t.bigint "event_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_sponsors_on_event_id"
